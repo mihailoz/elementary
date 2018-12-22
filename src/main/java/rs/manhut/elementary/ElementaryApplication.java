074a0a343761c26b34cf7a88ba1167fe13f70711
Mihailo Zdravkovic <mihailo.zdravkovic@gmail.com>
2018-12-22 12:39:51 +0100

    Initialize project

---
+++ src/main/java/rs/manhut/elementary/ElementaryApplication.java
@@ -0,0 +1,33 @@
+package rs.manhut.elementary;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+@SpringBootApplication
+@Controller
+@EnableAutoConfiguration(exclude={DataSourceAutoConfiguration.class})
+public class ElementaryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElementaryApplication.class, args);
+	}
+
+	@GetMapping("/resource")
+	@ResponseBody
+	public Map<String, Object> home() {
+		Map<String, Object> model = new HashMap<String, Object>();
+		model.put("id", UUID.randomUUID().toString());
+		model.put("content", "Hello World");
+		return model;
+	}
+}
+
